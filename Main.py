f54ba5e254a3b0dde821e2b8eb361d131576f8ef
grt812 <garrett.m.gee@gmail.com>
2023-11-04 15:53:56 -0400

    initial server setup

---
+++ Main.py
@@ -0,0 +1,21 @@
+
+# an object of WSGI application 
+from flask import Flask, send_from_directory
+
+
+app = Flask(__name__)   # Flask constructor 
+  
+# A decorator used to tell the application 
+# which URL is associated function 
+
+@app.route("/")
+def mainpage():
+     return send_from_directory("static", "index.html")
+
+
+@app.route('/')       
+def hello(): 
+    return 'HELLO'
+  
+if __name__=='__main__': 
+   app.run(host="0.0.0.0") 
