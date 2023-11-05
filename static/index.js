6fec98162e04a2b5abb82ba5524c5045c590f119
grt812 <garrett.m.gee@gmail.com>
2023-11-05 02:46:17 -0500

    update

--- static/index.js
+++ static/index.js
@@ -31,6 +31,13 @@ let categoryHTML = `
         </div>
     </div>
 `;
+//Dict
+//dict of assignments,
+    //in assignment: score, totalScore, (in "tuple")
+//category needed (predictedCategory): true/false
+    //if true: predicted category precentage
+
+
 $(function(){
     let dictOfCategories = {};
     $("#add-category").click(function(){
@@ -39,73 +46,122 @@ $(function(){
         $("#category-input")[0].focus();
     });
     $("#category-input").keypress(function(e){
-        if(e.keyCode == 13 && $("#category-input").val().length >=2){
+        let categoryValue = $("#category-input").val();
+        if(e.keyCode == 13 && $("#category-input").val().length >=2 && !dictOfCategories.hasOwnProperty(categoryValue)){
             $(this).hide(500);
             $("#add-category").show(500);
-            let categoryValue = $("#category-input").val();
-            if(!dictOfCategories.hasOwnProperty(categoryValue)){
-                dictOfCategories[categoryValue] = "";
-                $("#category-container").append(`
-                    <div id="category-${categoryValue}" class="category">
-                        <h1>${categoryValue}</h1>
-                        <div class="d-flex">
-                            <div class="left-col">
-                                <div id="score-container-${categoryValue}" class="score-container">
-                                </div>
-                                <div class="">
-                                    <button id="add-score-${categoryValue}">Add Assignment+</button>
-                                    <button id="add-predicted-${categoryValue}">Add Predicted Assignment+</button>
-                                </div>
+            dictOfCategories[categoryValue] = {"listOfScores":{}};
+            $("#category-container").append(`
+                <div id="category-${categoryValue}" class="category">
+                    <h1>${categoryValue}</h1>
+                    <div class="d-flex">
+                        <div class="left-col">
+                            <div id="score-container-${categoryValue}" class="score-container">
                             </div>
-                            <div class="right-col">
-                                Predict Category
-                                <label class="switch">
-                                    <input type="checkbox" checked>
-                                    <span class="slider round"></span>
-                                </label>
-                                <input type="text" placeholder="Predicted Category %">
+                            <div class="">
+                                <button id="add-score-${categoryValue}">Add Assignment+</button>
+                                <button id="add-predicted-${categoryValue}">Add Predicted Assignment+</button>
                             </div>
                         </div>
-                        <div>
-                            <button id="delete-category-${categoryValue}" class="delete-category"><span class="material-symbols-outlined"> close </span></button>
+                        <div class="right-col">
+                            Predict Category
+                            <label class="switch">
+                                <input class="predicted-${categoryValue}" type="checkbox" checked>
+                                <span class="slider round"></span>
+                            </label>
+                            <input id="predicted-num-${categoryValue}" type="text" placeholder="Predicted Category %">
                         </div>
                     </div>
+                    <div>
+                        <button id="delete-category-${categoryValue}" class="delete-category"><span class="material-symbols-outlined"> close </span></button>
+                    </div>
+                </div>
+            `);
+            $(`#predicted-num-${categoryValue}`).change(function(e){
+                dictOfCategories[categoryValue]["predictedValue"] = $(this).val();
+            });
+            $(`#predicted-${categoryValue}`).change(function(e){
+                if($(this).is(":checked")){
+                    dictOfCategories[categoryValue]["predictedCategory"] = true;
+                } else {
+                    dictOfCategories[categoryValue]["predictedCategory"] = false;
+                }
+            });
+            $("#delete-category-"+categoryValue).click(function(){
+                $(`#category-${categoryValue}`).hide(500);
+                let $this = $(this);
+                setTimeout(function(){
+                    $this.parents(`#category-${categoryValue}`).first().remove();
+                    console.log("")
+                }, 500);
+            });
+            $("#add-score-"+categoryValue).click(function(){
+                let scoreID = Date.now();
+                let specificID = `${categoryValue}-${scoreID}`;
+                let scoreNumerator = "numerator-" + specificId;
+                let scoreDenominator = "denominator-" + specificId;
+                $(`#score-container-${categoryValue}`).append(`
+                <div id="score-${specificID}" class="score">
+                    <input id=${scoreNumerator} class="" type="text" placeholder="Points Earned" size="14"> &nbsp;&nbsp;&nbsp;out of&nbsp; &nbsp;&nbsp;
+                    <input id=${scoreDenominator} class="" type="text" placeholder="Points Possible" size="14">&nbsp;&nbsp;&nbsp;
+                    <button id="delete-${specificID}">Delete</button>
+                </div>
                 `);
-                $("#delete-category-"+categoryValue).click(function(){
-                    $(`#category-${categoryValue}`).hide(500);
-                    let $this = $(this);
-                    setTimeout(function(){
-                        $this.parents(`#category-${categoryValue}`).first().remove();
-                        console.log("")
-                    }, 500);
+                $(`#score-${specificID}`).change(function(){
+                    dictOfCategories[categoryValue][specificID][0] = $("#"+scoreNumerator).val();
+                    dictOfCategories[categoryValue][specificID][1] = $("#"+scoreDenominator).val();
+                    predict_grade_parse(dictOfCategories, $("#start-date").val(), $("#end-date").val(), $("#grade-goal").val());
                 });
-                $("#add-score-"+categoryValue).click(function(){
-                    let scoreID = Date.now();
-                    $(`#score-container-${categoryValue}`).append(`
-                    <div id=score-${categoryValue}-${scoreID} class="score">
-                        <input id=${""} class="" type="text" placeholder="Points Earned" size="14"> &nbsp;&nbsp;&nbsp;out of&nbsp; &nbsp;&nbsp;
-                        <input id=${""} class="" type="text" placeholder="Points Possible" size="14">&nbsp;&nbsp;&nbsp;
-                        <button id="delete-${categoryValue}-${scoreID}">Delete</button>
-                    </div>
-                    `);
-                    $(`#delete-${categoryValue}-${scoreID}`).click(function(){
-                        $(this).parent().remove();
-                    });
-                });   
-                $("#add-category")[0].focus();
-                $("#category-input").val("");
-            } 
+                $(`#delete-${specificID}`).click(function(){
+                    $(this).parent().remove();
+                    delete dictOfCategories[categoryValue];
+                    predict_grade_parse(dictOfCategories, $("#start-date").val(), $("#end-date").val());
+                });
+            });   
+            $("#add-category")[0].focus();
+            $("#category-input").val("");
         }
     });
-        
-    });
-
-    $("").keydown(function(event) {
-        /*
-         * update dictionary
-         */
-        predict_grade();
 });
+
+
+//Dict of categories (each key is an id):
+    //dict of assignments (each key is an id):
+        //in assignment: score, totalScore, (in "tuple" indexes 0 and 1), (score -1 = future assignment)
+    //category needed (predictedCategory): true/false
+    //predicted category (predictedValue) value
+
+
+function predict_grade_parse(dictionary, startDate, endDate, gradeGoal){
+	let start = new Date(startDate);
+	let end = new Date(endDate);
+	let semesterProgress = (Date.now() - start.parse())/(end.parse()-start.parse());
+
+	let gradeables = {};
+	let futures = {};
+	let remaining_avg = {};
+	let predict_cats = [];
+
+	for(let cat in dictionary){
+		gradeables.set(cat, []);
+		futures.set(cat, []);
+		remaining_avg.set(cat, dictionary[cat][2]);
+		for(let asig in cat){
+			if(dictionary[cat][asig][0]==-1){
+				gradeables[cat].push([dictionary[cat][asig][0], dictionary[cat][asig][1]]);
+			}
+			else{
+				futures[cat].push(dictionary[cat][asig][1]);
+			}
+		}
+		if(dictionary[cat][1]){
+			predict_cats.push(cat);
+		}
+	}
+	
+	predict_grade(semesterProgress, gradeables, futures, remaining_avg, predict_cats, gradeGoal);
+}
+
 
 
 function predict_grade(
@@ -113,7 +169,7 @@ function predict_grade(
 	all_gradeables, // dictionary of categories -> list of tuples (points earned, points possible)
 	future_assignments, // dictionary of categories -> list of (points possible)
 	remaining_avg, // dictionary of categories -> avg score expected for that category
-	predict_category, // string indicating which category to predict grades for
+	predict_categories, // list of strings indicating which category to predict grades for
 	grade_goal // grade user wants to get
 ) {
 	let tot_points = 0;
@@ -127,7 +183,7 @@ function predict_grade(
 			asig_score += all_gradeables[cat][asig][0];
 			asig_possible += all_gradeables[cat][asig][1];
 		}
-      	if(cat==predict_category){
+			if(predict_categories.includes(cat)){
 			continue;
 		}
 		if(future_assignments[cat].length==0){
@@ -140,23 +196,31 @@ function predict_grade(
 				asig_possible += future_assignments[cat][asig];
 			}
 		}
-		possible_subscore += asig_possible;
 		expected_subscore += asig_score;
+			possible_subscore += asig_possible;
 		tot_points += asig_possible;
 	}
-	
-	for(let asig in all_gradeables[predict_category]){
-		tot_points += all_gradeables[predict_category][asig][1];
+	for(let cat in predict_categories) {
+		let cur_predict_score = 0;
+		for(let asig in all_gradeables[predict_categories[cat]]){
+			expected_subscore += all_gradeables[predict_categories[cat]][asig][0];
+			possible_subscore += all_gradeables[predict_categories[cat]][asig][1];
+			tot_points += all_gradeables[predict_categories[cat]][asig][1];
+			cur_predict_score += all_gradeables[predict_categories[cat]][asig][1];
+		}
+			if(future_assignments[predict_categories[cat]].length==0){
+			tot_points += cur_predict_score / semester_progress * (1-semester_progress);
+		}
+		else{
+			for(let asig in future_assignments[predict_categories[cat]]){
+				tot_points += future_assignments[predict_categories[cat]][asig];
+			}
+		}
 	}
 
 	let required_tot = tot_points * grade_goal;
 	let required_rem = required_tot - expected_subscore;
 	let required_avg = required_rem / (tot_points - possible_subscore);
-      console.log(tot_points);
-      console.log(required_tot);
-      console.log(possible_subscore);
-      console.log(expected_subscore);
-      console.log(required_rem);
-
+		
 	return required_avg;
 }
